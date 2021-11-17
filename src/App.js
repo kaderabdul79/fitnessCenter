bc8b97c0c30ae21da0542393dace80ae6c0354f6
kaderabdul79 <kaderabdul7980@gmail.com>
2021-11-17 10:33:27 +0600

    fetch data from localhost and show

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState , useEffect } from 'react';
 
 function App() {
+  const [users,setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:4000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{users.length}</h2>
+      {
+        users.map((user) => {
+          <h1>{user.name}</h1>
+          console.log(user.name)
+        })
+      }
+      {/* <ul>
+        {
+          users.map(user => {
+            <li key={user.id}>{user.id} {user.Address}g</li>
+          })
+        }
+      </ul> */}
     </div>
   );
 }
