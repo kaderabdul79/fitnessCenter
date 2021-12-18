926ba55adc8bcc6f9cb1a03fe6fec87894fb18b0
kaderabdul79 <kaderabdul7980@gmail.com>
2021-12-18 23:29:01 +0600

    login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h4>Please Login with</h4>
+            <button>Login using Google</button>
+        </div>
+    );
+};
+
+export default Login;
