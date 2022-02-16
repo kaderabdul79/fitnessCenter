09a1dd7e4a8ef91c11c447d35137da8dbcb58bcc
kaderabdul <kaderabdul7980@gmail.com>
2022-02-17 00:13:28 +0600

    course description show only 100 char

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -11,17 +11,8 @@ const Course = (props) => {
                     <img src={img} alt="" />
                 </div>
                 <h2>{title}</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore voluptatem, facilis fugiat laudantium modi molestias accusantium enim, omnis doloremque dolores neque explicabo mollitia quas consectetur, magni beatae accusamus hic ex</p>
+                <p>{(description.length > 100) ? description.substring(0,100) : description}</p>
                 <button className='btnstyle'><Link to={`/courses/${slug.toLowerCase()}`}>Learn more</Link></button>
-                
-            {/* <div className="course-img">
-                <img src={img} alt="" />
-            </div>
-            <div className="course-content">
-                <h2>{title}</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore voluptatem, facilis fugiat laudantium modi molestias accusantium enim, omnis doloremque dolores neque explicabo mollitia quas consectetur, magni beatae accusamus hic ex</p>
-                <button>learn more</button>
-            </div> */}
         </div>
     );
 };
