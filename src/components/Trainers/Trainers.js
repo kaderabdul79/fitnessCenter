0eea01a6a9065e7ce8b02f2e8d4e0b9308a4d8ef
kaderabdul <kaderabdul7980@gmail.com>
2022-02-17 11:33:21 +0600

    fetch trainers data from db and show the ui

--- src/components/Trainers/Trainers.js
+++ src/components/Trainers/Trainers.js
@@ -1,50 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Trainers.css';
 
 const Trainers = () => {
+ 
+    const [trainers,setTrainers] = useState([])
+    
+    // load data from own api
+    useEffect(()=>{
+        fetch('http://localhost:5000/trainers')
+        .then(res => res.json())
+        .then(data => setTrainers(data))
+    },[])
+
+    // console.log(trainers)
     return (
         <div className='trainers-area'>
-            <div className="trainer">
-                <div className="trainers-img">
-                    <img src="https://fitness.wpmix.net/wp-content/uploads/2017/06/single-trainer.jpg" alt="" />
-                </div>
-                <div className="trainers-info">
-                    <h3>JULIA JONES</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <button className='btnstyle'><Link to={`/trainers}`}>view profile</Link></button>
-                </div>
-            </div>
-            <div className="trainer">
-                <div className="trainers-img">
-                    <img src="https://fitness.wpmix.net/wp-content/uploads/2017/06/single-trainer.jpg" alt="" />
-                </div>
-                <div className="trainers-info">
-                    <h3>JULIA JONES</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <button className='btnstyle'><Link to={`/trainers}`}>view profile</Link></button>
-                </div>
-            </div>
-            <div className="trainer">
-                <div className="trainers-img">
-                    <img src="https://fitness.wpmix.net/wp-content/uploads/2017/06/single-trainer.jpg" alt="" />
-                </div>
-                <div className="trainers-info">
-                    <h3>JULIA JONES</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <button className='btnstyle'><Link to={`/trainers}`}>view profile</Link></button>
-                </div>
-            </div>
-            <div className="trainer">
-                <div className="trainers-img">
-                    <img src="https://fitness.wpmix.net/wp-content/uploads/2017/06/single-trainer.jpg" alt="" />
-                </div>
-                <div className="trainers-info">
-                    <h3>JULIA JONES</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <button className='btnstyle'><Link to={`/trainers}`}>view profile</Link></button>
-                </div>
-            </div>
+              {
+                  trainers.map((trainer,index) => (
+                  <div className="trainer" key={index}>
+                    <div className="trainers-img">
+                        <img src={trainer.img} alt="trainers image" />
+                    </div>
+                    <div className="trainers-info">
+                        <h3>{trainer.name}</h3>
+                        <p>{trainer.Classes}</p>
+                        <button className='btnstyle'><Link to={`/trainers}`}>view profile</Link></button>
+                    </div>
+                   </div>
+                  ))
+              }
         </div>
     );
 };
